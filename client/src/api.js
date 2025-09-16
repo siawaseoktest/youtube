850cf9bbb0411ae5a0dbfb7ee54dcb91414a04be
ajgpw <156731346+ajgpw@users.noreply.github.com>
2025-09-16 01:24:41 +0000

    apiリスト更新

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 // 複数のAPIエンドポイント
 const API_URLS = [
-  "https://script.google.com/macros/s/AKfycbzqpav7y2x3q756wRSOhBzaXf-2hKaLTvxoFN8kFegrIvamH03ZXphEw2PK30L7AstC/exec",
+  "https://script.google.com/macros/s/AKfycbynFMFgEAEK_XwigztO9VnGdTt2KtMtLoNZiR4H-lsHen6RdqNm32WGTDggZ2PC1D9U/exec",
   "https://script.google.com/macros/s/AKfycbzvFclFgt2ys3RDp89L0Tdntsdbgr2iSnnwHcJhsydL3lBWK1nTTT-Ya28xoDl7wWFATg/exec",
   "https://script.google.com/macros/s/AKfycbxdoa669pQcZD_-U4lTvHP19q7XvtWONJ6RMTd9-qfmLksqIp7MUfuySQ3odr_1hWXuQA/exec"
 ];
